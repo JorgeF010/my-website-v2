f0eef59a99ca8ab86c525c309a173c0f9af9c625
JorgeF010 <46510272+JorgeF010@users.noreply.github.com>
2020-12-31 06:32:01 -0500

    Created an Express Server
    - added an express server to add the functionality for the contact server
    - moved files around to account for the server / ui

---
+++ website-client/src/App - Copy.js
@@ -0,0 +1,51 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from "./components/Navbar";
+import Home from './components/pages/Home';
+import Footer from './components/Footer';
+import Contact from './components/pages/Contact';
+import Projects from './components/pages/Projects';
+import About from './components/pages/About';
+
+function App() {
+
+  state = {
+    data: null
+  };
+
+  componentDidMount() {
+      // Call our fetch function below once the component mounts
+    this.callBackendAPI()
+      .then(res => this.setState({ data: res.express }))
+      .catch(err => console.log(err));
+  }
+    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  callBackendAPI = async () => {
+    const response = await fetch('/express_backend');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message) 
+    }
+    return body;
+  };
+
+
+  return (
+    <>
+      <Router>
+        <Navbar/>
+        <Switch>
+          <Route path='/' exact component={Home}/>
+          <Route path='/projects' exact component={Projects}/>
+          <Route path='/about' exact component={About}/>
+          <Route path='/contact' exact component={Contact}/>
+        </Switch>
+        <h2 className='App-intro'>{this.state.data}</h2>
+        <Footer/>
+      </Router>
+    </>
+  );
+}
+
+export default App;
