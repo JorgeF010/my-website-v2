096acb15ee84eee55b7502177488673ab475cf29
JorgeF010 <46510272+JorgeF010@users.noreply.github.com>
2021-01-02 05:43:46 -0500

    Working on the Server + App
    made a few minor changes

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 5000;
+// SendGrid API
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-// console.log that your server is up and running
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const msg = {
+  to: 'figueroajorge010@gmail.com', // Change to your recipient
+  from: 'softwarebyjorge@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
 
-// create a GET route
-app.get('/express_backend', (req, res) => {
-  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-});
+
+
+// // console.log that your server is up and running
+// app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// // create a GET route
+// app.get('/express_backend', (req, res) => {
+//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// });
