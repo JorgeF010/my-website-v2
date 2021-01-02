920358ea23a3d3c3e55378516dc688ec78696172
jlf4509 <jlf4509@g.rit.edu>
2021-01-02 07:44:10 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-// const express = require('express');
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-// SendGrid API
+const express = require('express');
+const cors = require('cors');
 const sgMail = require('@sendgrid/mail');
+
+const app = express();
+// SendGrid API
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+app.use(cors());
+const port = process.env.PORT || 5000;
 
-const msg = {
-  to: 'figueroajorge010@gmail.com', // Change to your recipient
-  from: 'softwarebyjorge@gmail.com', // Change to your verified sender
-  subject: 'Testing Email',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-}
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent')
-  })
-  .catch((error) => {
-    console.error(error)
-  })
+// welcome page
+app.get('/', (req, res) => {
+  res.send('Email Server - Using SendGrid')
+});
 
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
+// create a GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
 
-// // console.log that your server is up and running
-// app.listen(port, () => console.log(`Listening on port ${port}`));
-
-// // create a GET route
-// app.get('/express_backend', (req, res) => {
-//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
-// });
+// const msg = {
+//   to: 'figueroajorge010@gmail.com', // Change to your recipient
+//   from: 'softwarebyjorge@gmail.com', // Change to your verified sender
+//   subject: 'Testing Email',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// }
+// sgMail
+//   .send(msg)
+//   .then(() => {
+//     console.log('Email sent')
+//   })
+//   .catch((error) => {
+//     console.error(error)
+//   })
